0bf137a89d45290d90a8ad9aa2fdfb42ef69acab
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-09 11:23:37 -0500

    angular hello world

--- public/main.js
+++ public/main.js
@@ -1,1 +1,6 @@
-console.log("Works!");
+angular.module("app", [])   //NOTE(adam): array here causes instantiation of module
+  .controller("MainCtrl", function() {
+    const main = this;
+
+    main.heading = "Rock the Vote!";
+  })
