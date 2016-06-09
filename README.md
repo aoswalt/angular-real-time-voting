a88892bb4f920d1188ac72f3721103ff30eadc5b
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-09 13:27:03 -0500

    restart

--- README.md
+++
@@ -1,1 +0,0 @@
-# angular-real-time-voting
--- public/main.js
+++
@@ -1,6 +0,0 @@
-angular.module("app", [])   //NOTE(adam): array here causes instantiation of module
-  .controller("MainCtrl", function() {
-    const main = this;
-
-    main.heading = "Rock the Vote!";
-  })
