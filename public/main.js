9aa9b580b60586238679849564ffbe17837304ac
Adam Oswalt <adam.oswalt@gmail.com>
2016-06-09 13:51:20 -0500

    read from firebase

--- public/main.js
+++ public/main.js
@@ -1,6 +1,16 @@
 angular.module("app", [])
-  .controller("MainCtrl", function() {
+  .config(() => {
+    firebase.initializeApp({
+      apiKey: "AIzaSyAT2D9QJIiPHzlORB8fGEoRQQAPoeazwn4",
+      authDomain: "angular-real-time-voting.firebaseapp.com",
+      databaseURL: "https://angular-real-time-voting.firebaseio.com",
+      storageBucket: "angular-real-time-voting.appspot.com"
+    });
+  })
+
+  .controller("MainCtrl", function($timeout) {
     const main = this;
 
     main.heading = "Voterific";
+    firebase.database().ref("/").once("value", snapshot => $timeout(main.heading = snapshot.val()));
   });
